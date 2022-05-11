9cd8ef970fb0d361c2b09325b372c93fba52803f
Gaël <gael77@hotmail.fr>
2022-05-11 16:59:22 +0200

    CV + responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,21 @@ const Home = () => {
   return (
     <div className="home">
       <TypedText string={[`Bienvenue sur ma page.`,`Bienvenue chez moi.<br />^1600 C'est ici que naissent mes rêves.`]} />
+      <div className="network">
+        <button>
+          <a href="https://github.com/glg-dev" target="_blank" rel='noopener noreferrer'>
+            <i className='fab fa-github'></i>
+          </a>
+        </button>
+        <button>
+          <a href="https://www.linkedin.com/in/ga%C3%ABl-lecoq-965484235/" target="_blank" rel='noopener noreferrer'>
+            <i className='fab fa-linkedin'></i>
+          </a>
+        </button>
+        <button>
+          <a href="./assets/CV.pdf" target='_blank' rel='noopener noreferrer'>Voir mon CV</a>
+        </button>
+      </div>
     </div>
   );
 };
